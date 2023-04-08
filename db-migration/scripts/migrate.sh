644c932229cc05d3b7529a648820eea824026c11
triceratopss <96670142+triceratopss@users.noreply.github.com>
2023-04-08 09:05:17 +0900

    fix

--- db-migration/scripts/migrate.sh
+++ db-migration/scripts/migrate.sh
@@ -6,7 +6,7 @@ ls ../keys
 curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.1.2/cloud-sql-proxy.linux.amd64
 chmod +x cloud-sql-proxy
 
-./cloud-sql-proxy --port 5432 --credentials-file ../keys/SERVICE_ACCOUNT_KEY_FILE ${INSTANCE_CONNECTION_NAME} & sleep 2;
+./cloud-sql-proxy --port 5432 --private-ip --credentials-file ../keys/SERVICE_ACCOUNT_KEY_FILE ${INSTANCE_CONNECTION_NAME} & sleep 2;
 
 URL="postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?search_path=public&sslmode=disable"
 
